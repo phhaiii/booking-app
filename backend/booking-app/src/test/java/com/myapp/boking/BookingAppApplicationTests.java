5d907c648b17c5de1611cfa9222096ba1e3fc845
phhaiii <phuhai6@gmail.com>
2025-09-29 01:24:35 +0700

    first commit

---
+++ backend/booking-app/src/test/java/com/myapp/boking/BookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.boking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
